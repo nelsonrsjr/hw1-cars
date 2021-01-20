c514f34d8cd268d5fac03865f52446db01b8373c
nelsonrsjr <nelsonrs@hawaii.edu>
2021-01-19 23:01:56 -1000

    Submission #1

--- cars_script.R
+++ cars_script.R
@@ -13,3 +13,4 @@ par(mfrow=c(2,2))   # this sets the plot environment to do 4 plots, 2 by 2
 plot(speed.lm)  # the default plot method for lm - crazy huh?
 
 write.csv(cars, "cars.csv")  # save data to csv file in working directory
+
